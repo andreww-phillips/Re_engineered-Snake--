0810da8f56252ebb4302517c3b1ce28fa53ef92b
andreww-phillips <andrew.phillips004@gmail.com>
2026-03-29 20:58:21 -0400

    finished food class

--- Snake/Food.cpp
+++ Snake/Food.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <deque>
+#include <cstdlib>
 
 class Food{
     struct Vec2 { //Creates a struct for the snake/body and its direction
@@ -12,7 +13,22 @@ class Food{
     public:
         Vec2 position;
 
-        void spawn(){
+        // Generate food at random positions within the grid, making sure they're not in positions occupied
+        // by the snake's body
+        void Food::spawn(const std::deque<Vec2>& body){
+            do{
+                position.x = rand() % 20;
+                position.y = rand() % 20;
+            }while(isOnSnake(body));
+        }
 
+        // Instance checking for food spawning to ensure that the food does not spawn on the snake's body
+        bool isOnSnake(const std::deque<Vec2>& body){
+            for(const Vec2& segment : body){
+                if(position == segment){
+                    return true;
+                }
+            }
+            return false;
         }
 };
